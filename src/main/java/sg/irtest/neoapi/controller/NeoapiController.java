4193f6c57e67e8cf67c300c8eedab85538a50263
amridio <kawir23@gmail.com>
2024-05-27 00:29:42 +0700

    Add request param on Get Controller

--- src/main/java/sg/irtest/neoapi/controller/NeoapiController.java
+++ src/main/java/sg/irtest/neoapi/controller/NeoapiController.java
@@ -2,6 +2,7 @@ package sg.irtest.neoapi.controller;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import sg.irtest.neoapi.NeoapiClient;
@@ -14,9 +15,10 @@ public class NeoapiController {
     @Value("${nasa.apikey}") String apiKeyClient;
 
     @GetMapping("/top10neo")
-    public String top10neo() {
+    public String top10neo(@RequestParam(value = "start_date", required = true) String startDate,
+            @RequestParam(value = "end_date", required = true) String endDate) {
         NeoapiClient neoApiClient = new NeoapiClient(this.URLClient, this.apiKeyClient);
-        String resp = neoApiClient.neodata("2023-04-12", "2023-04-12");
+        String resp = neoApiClient.neodata(startDate, endDate);
         return resp;
     }
     
