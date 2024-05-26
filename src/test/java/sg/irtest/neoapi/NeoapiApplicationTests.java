3750ab1cf778542124eebbe91cf9664fa266fc50
amridio <kawir23@gmail.com>
2024-05-26 18:50:20 +0700

    Initial creating project commit

---
+++ src/test/java/sg/irtest/neoapi/NeoapiApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.irtest.neoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeoapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
