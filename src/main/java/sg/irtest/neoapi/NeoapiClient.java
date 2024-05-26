98068cc3679ac437751e96c0d565512a2e48ee29
amridio <kawir23@gmail.com>
2024-05-27 00:17:07 +0700

    Adding client class to manage client request to NASA API

--- src/main/java/sg/irtest/neoapi/NeoapiClient.java
+++ src/main/java/sg/irtest/neoapi/NeoapiClient.java
@@ -1,25 +1,27 @@
 package sg.irtest.neoapi;
 
 import org.springframework.web.reactive.function.client.WebClient;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.env.Environment;
-
-
 
 public class NeoapiClient {
     
-    @Autowired
-    private Environment env;
 
-    private final String apiUrl = env.getProperty("nasa.baseurl");
-    private final String apiKey = env.getProperty("nasa.apikey");
+    private final String apiUrl;
+    private final String apiKey;
     private final WebClient webClient;
 
-    public NeoapiClient() {
+    public NeoapiClient(String url, String key) {
+        this.apiUrl =  url;
+        this.apiKey =  key;
         this.webClient = WebClient.builder().baseUrl(apiUrl).build();
-        System.out.println("URL:" + apiUrl);
-        System.out.println("ApiKey:" + apiKey);
     }
 
+    public String neodata(String startDate, String endDate) {
+        String resp = webClient.get()
+        .uri(uriBuilder -> uriBuilder.path("feed").queryParam("start_date",startDate).queryParam("end_date", endDate).queryParam("api_key", apiKey).build())
+        .retrieve()
+        .bodyToMono(String.class)
+        .block();
+        return new String(resp);
+    }
 
 }
--- src/main/java/sg/irtest/neoapi/controller/NeoapiController.java
+++ src/main/java/sg/irtest/neoapi/controller/NeoapiController.java
@@ -1,8 +1,23 @@
 package sg.irtest.neoapi.controller;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import sg.irtest.neoapi.NeoapiClient;
 
 @RestController
 public class NeoapiController {
+
+    @Value("${nasa.baseurl}") String URLClient;
+
+    @Value("${nasa.apikey}") String apiKeyClient;
+
+    @GetMapping("/top10neo")
+    public String top10neo() {
+        NeoapiClient neoApiClient = new NeoapiClient(this.URLClient, this.apiKeyClient);
+        String resp = neoApiClient.neodata("2023-04-12", "2023-04-12");
+        return resp;
+    }
     
 }
